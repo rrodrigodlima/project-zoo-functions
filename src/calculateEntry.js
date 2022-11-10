aa1bd94b4cffe7472ee945cebef6f9bbb51e8681
rrodrigodlima <rdrigo019@gmail.com>
2022-11-10 20:38:29 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,30 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  let childCounter = 0;
+  let adultCounter = 0;
+  let seniorCounter = 0;
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      childCounter += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      adultCounter += 1;
+    } else {
+      seniorCounter += 1;
+    }
+  });
+  return { child: childCounter, adult: adultCounter, senior: seniorCounter };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  
+  const priceChild = countEntrants(entrants).child * data.prices.child;
+  const priceAdult = countEntrants(entrants).adult * data.prices.adult;
+  const priceSenior = countEntrants(entrants).senior * data.prices.senior;
+  return priceChild + priceAdult + priceSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
