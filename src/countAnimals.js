46ef9a17a66d66159e5923fafd0acd424eebf740
rrodrigodlima <rdrigo019@gmail.com>
2022-11-10 20:28:26 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const { specie } = animal;
+  if (Object.keys(animal).length === 1) {
+    return data.species.find(({ name }) => name === specie).residents.length;
+  } if (Object.keys(animal).length === 2) {
+    return data.species.find(({ name }) => name === specie)
+      .residents.filter(({ sex }) => sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
