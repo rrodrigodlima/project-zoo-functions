c4970f0c328ca89e25af9aa6c16c6c27f7d09afd
rrodrigodlima <rdrigo019@gmail.com>
2022-11-11 02:55:28 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const getInfo = data.employees.map((info) => ({
+  id: info.id,
+  fullName: `${info.firstName} ${info.lastName}`,
+  species: info.responsibleFor.map((id) => data.species.find((specie) => specie.id === id).name),
+  locations: info.responsibleFor.map((id) =>
+    data.species.find((specie) => specie.id === id).location),
+}));
+
+const getEmployeesCoverage = (param) => {
+  if (param === undefined) {
+    return getInfo;
+  }
+  const validation = getInfo.find((info) =>
+    info.fullName.includes(Object.values(param)) || info.id.includes(Object.values(param)));
+  if (validation === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return validation;
+};
 
 module.exports = getEmployeesCoverage;
