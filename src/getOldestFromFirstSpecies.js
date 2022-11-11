71afbaf6a825640fc8277c8751d71e3ebbf6d804
rrodrigodlima <rdrigo019@gmail.com>
2022-11-11 02:38:34 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,6 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-}
+const getOldestFromFirstSpecies = (id) => {
+  const firstSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animals = data.species.find((specie) => specie.id === firstSpecie).residents;
+  const animalsAges = animals.sort((a, b) => a.age - b.age);
+return Object.values(animalsAges[animalsAges.length - 1]);
+};
 
 module.exports = getOldestFromFirstSpecies;
