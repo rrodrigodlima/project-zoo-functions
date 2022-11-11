751b43f2384772eea997b33ed28fb575f75ca00e
rrodrigodlima <rdrigo019@gmail.com>
2022-11-11 03:54:15 -0300

    test coverage requisitos 6,7,12,13,15,16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,51 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica se caso não seja passado nenhum argumento retorna o objeto', () => {
+    expect(getOpeningHours()).toMatchObject({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+
+  it('Verifica se retorna a string -The zoo is closed-', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+
+  it('Verifica se retorna a string -The zoo is open-', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+
+  it('Verifica se retorna a string -The zoo is closed-', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+
+  it('Verifica se retorna a mensagem 1', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow(/^The day must be valid. Example: Monday$/);
+  });
+
+  it('Verifica se retorna a mensagem 2', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow(/^The abbreviation must be 'AM' or 'PM'$/);
+  });
+
+  it('Verifica se retorna a mensagem 3', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00AM')).toThrow(/^The hour should represent a number$/);
+  });
+
+  it('Verifica se retorna a mensagem 4', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow(/^The minutes should represent a number$/);
+  });
+
+  it('Verifica se retorna a mensagem 5', () => {
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow(/^The hour must be between 0 and 12$/);
+  });
+
+  it('Verifica se retorna a mensagem 6', () => {
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow(/^The minutes must be between 0 and 59$/);
+  });
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,39 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se retorna o número inteiro 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('Verifica se retorna um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+
+  it('Verifica se retorna um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+  it('Verifica se retorna a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  it('Verifica se retorna um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+
+  it('Verifica se retorna um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+
+  it('Verifica se retorna undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+
+  it('Verifica se retorna a mensagem de erro', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Verifica se o argumento passado não contemplar nenhuma funcionalidade retorna null', () => {
+    expect(handlerElephants('random')).toBe(null);
+  });
+});
